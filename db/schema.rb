28119834556bba7d7c5d7b829ea7e2115a343ecc
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-03 18:09:16 +0900

    businessモデル、テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_084232) do
+ActiveRecord::Schema.define(version: 2019_09_03_090405) do
 
   create_table "business_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2019_09_03_084232) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_business_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_business_users_on_reset_password_token", unique: true
+  end
+
+  create_table "businesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "phone_num", null: false
+    t.string "capital"
+    t.string "type", null: false
+    t.string "url"
+    t.text "text"
+    t.bigint "business_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_user_id"], name: "index_businesses_on_business_user_id"
   end
 
   create_table "consumers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -49,5 +61,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_084232) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "businesses", "business_users"
   add_foreign_key "consumers", "users"
 end
