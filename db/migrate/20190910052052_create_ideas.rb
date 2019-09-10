b52ed91f3255b033f63c16ca4f5af49b0ceb5c7c
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-10 14:31:22 +0900

    ideasテーブルの作成

--- db/migrate/20190910052052_create_ideas.rb
+++ db/migrate/20190910052052_create_ideas.rb
@@ -1,7 +1,11 @@
 class CreateIdeas < ActiveRecord::Migration[5.2]
   def change
     create_table :ideas do |t|
-
+      t.string      :name, null: false
+      t.string      :url
+      t.text        :text
+      t.references  :consumer, foreign_key: true
+      t.references  :business, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_090405) do
+ActiveRecord::Schema.define(version: 2019_09_10_052052) do
 
   create_table "business_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 2019_09_03_090405) do
     t.index ["user_id"], name: "index_consumers_on_user_id"
   end
 
+  create_table "ideas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "url"
+    t.text "text"
+    t.bigint "consumer_id"
+    t.bigint "business_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_id"], name: "index_ideas_on_business_id"
+    t.index ["consumer_id"], name: "index_ideas_on_consumer_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -63,4 +75,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_090405) do
 
   add_foreign_key "businesses", "business_users"
   add_foreign_key "consumers", "users"
+  add_foreign_key "ideas", "businesses"
+  add_foreign_key "ideas", "consumers"
 end
