bef2e5a0b34c1038169fc16e211ba3b541cc785a
tsuyoshit201603 <tsuyoshi.t201603@gmail.com>
2019-09-10 14:01:48 +0900

    business情報登録機能の実装

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,17 @@
 class BusinessesController < ApplicationController
   def new
+    @business = Business.new
+  end
+
+  def create
+    business = Business.new(business_params)
+    business.save
+    redirect_to root_path
+  end
+
+  private
+
+  def business_params
+    params.required(:business).permit(:phone_num, :capital, :type, :text).merge(business_user_id: params[:business_user_id])
   end
 end
